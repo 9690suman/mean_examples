07205d0c1461b8958e9556fd22e018e4ce3e2ad6
9690suman <sarnendu.mukherjee@gmail.com>
2015-09-18 16:22:09 +0530

    changes regarding front end angular coding.

--- EMS/app/controllers/index.server.controller.js
+++ EMS/app/controllers/index.server.controller.js
@@ -1,4 +1,4 @@
 exports.index = function(req,res){
 	// load the single view file (angular will handle the page changes on the front-end)
-	res.sendfile('./public/index.html'); 
+	res.sendfile('./public/app/index.html'); 
 }
